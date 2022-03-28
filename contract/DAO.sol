9334ad0f65f7d714f37295c35feb3c5342dc7f1f
hostlime <hostlime@ya.ru>
2022-03-28 08:38:17 +0300

    create project

---
+++ contract/DAO.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.4;
+
+
+contract DAO {
+
+    constructor (address _Token, uint256 _minimumQuorum, uint256 _debPerDuration, uint256 _requisiteMajority) {
+
+    }
+}
